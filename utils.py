688b69bee71c330d6d833a3c135cd9f73a28d944
cc-qy <qqndqy@gmail.com>
2021-06-15 20:29:09 +0800

    Add files via upload

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import copy
 import numpy.ma as ma
 from PIL import Image
+import torch
 # import numpy as np
 from sklearn.metrics import confusion_matrix as CM
 
@@ -503,60 +504,65 @@ def cal_one_pose_matrix(pose):
     pose_matrix = torch.cat((torch.cat((rotMat, translation), 2), filler), 1)
     return pose_matrix
 
-def quat2mat(q):
-    ''' Calculate rotation matrix corresponding to quaternion
-    https://afni.nimh.nih.gov/pub/dist/src/pkundu/meica.libs/nibabel/quaternions.py
-    Parameters
-    ----------
-    q : 4 element array-like
+def euler2mat(pose, isRadian=True):
+    def _euler_to_mat(z, y, x):
+        # x = rot_euler[:, 0]
+        # y = rot_euler[:, 1]
+        # z = rot_euler[:, 2]
 
-    Returns
-    -------
-    M : (3,3) array
-      Rotation matrix corresponding to input quaternion *q*
+        zeros = torch.zeros_like(x)  #(B,1)
+        # print (z.shape, zeros.shape)
+        ones = torch.ones_like(x)
 
-    Notes
-    -----
-    Rotation matrix applies to column vectors, and is applied to the
-    left of coordinate vectors.  The algorithm here allows non-unit
-    quaternions.
+        cosz = torch.cos(z)
+        sinz = torch.sin(z)
+        rotz_1 = torch.cat([cosz, -sinz, zeros], 1).unsqueeze(1) #(B,1,3)
+        rotz_2 = torch.cat([sinz, cosz, zeros], 1).unsqueeze(1)
+        rotz_3 = torch.cat([zeros, zeros, ones], 1).unsqueeze(1)
+        zmat = torch.cat((rotz_1, rotz_2, rotz_3), 1)   #(b,3,3)
+        # print (zmat.shape, zeros.shape)
 
-    References
-    ----------
-    Algorithm from
-    http://en.wikipedia.org/wiki/Rotation_matrix#Quaternion
+        cosy = torch.cos(y)
+        # print ("cos:",cosy[0])
+        siny = torch.sin(y)
+        roty_1 = torch.cat([cosy, zeros, siny], 1).unsqueeze(1)
+        roty_2 = torch.cat([zeros, ones, zeros], 1).unsqueeze(1)
+        roty_3 = torch.cat([-siny, zeros, cosy], 1).unsqueeze(1)
+        ymat = torch.cat((roty_1, roty_2, roty_3), 1)
 
-    Examples
-    --------
-    >>> import numpy as np
-    >>> M = quat2mat([1, 0, 0, 0]) # Identity quaternion
-    >>> np.allclose(M, np.eye(3))
-    True
-    >>> M = quat2mat([0, 1, 0, 0]) # 180 degree rotn around axis 0
-    >>> np.allclose(M, np.diag([1, -1, -1]))
-    True
-    '''
-    w, x, y, z = q
-    Nq = w * w + x * x + y * y + z * z
-    if Nq < 1e-8:
-        return np.eye(3)
-    s = 2.0 / Nq
-    X = x * s
-    Y = y * s
-    Z = z * s
-    wX = w * X
-    wY = w * Y
-    wZ = w * Z
-    xX = x * X
-    xY = x * Y
-    xZ = x * Z
-    yY = y * Y
-    yZ = y * Z
-    zZ = z * Z
-    return np.array([[1.0 - (yY + zZ), xY - wZ, xZ + wY],
-                     [xY + wZ, 1.0 - (xX + zZ), yZ - wX],
-                     [xZ - wY, yZ + wX, 1.0 - (xX + yY)]])
+        cosx = torch.cos(x)
+        sinx = torch.sin(x)
+        rotx_1 = torch.cat([ones, zeros, zeros], 1).unsqueeze(1)
+        rotx_2 = torch.cat([zeros, cosx, -sinx], 1).unsqueeze(1)
+        rotx_3 = torch.cat([zeros, sinx, cosx], 1).unsqueeze(1)
+        xmat = torch.cat((rotx_1, rotx_2, rotx_3), 1)
+        # rotMat = torch.matmul(zmat, torch.matmul(ymat, xmat))
+        rotMat = torch.matmul(torch.matmul(xmat, ymat), zmat)
 
+        return rotMat
+    z = np.zeros_like(pose[:,0])
+    y = np.zeros_like(pose[:,1])
+    x = np.zeros_like(pose[:,2])
+    if not isRadian:
+        z = ((np.pi) / 180.) * pose[:,0]
+        y = ((np.pi) / 180.) * pose[:,1]
+        x = ((np.pi) / 180.) * pose[:,2]
+    assert (z >= (-np.pi)).all() and (z < np.pi).all(), 'Inapprorpriate z: %f' % z
+    assert (y >= (-np.pi)).all() and (y < np.pi).all(), 'Inapprorpriate y: %f' % y
+    assert (x >= (-np.pi)).all() and (x < np.pi).all(), 'Inapprorpriate x: %f' % x
+    B,_ = pose.shape
+    pose = torch.from_numpy(pose.astype(np.float64))
+    rotMat = _euler_to_mat(pose[:,0:1], pose[:,1:2], pose[:,2:3])
+    # return rotMat
+    # print("rotMat:  ", rotMat.shape)
+    # xxx
+    translation = (pose[:, 3:]).unsqueeze(2)
+    filler = torch.tensor([[[0.0, 0.0, 0.0, 1.0]]])
+    filler = filler.repeat(B,1,1).double()
+    # print("filler:  ", filler.shape)
+    pose_matrix = torch.cat((torch.cat((rotMat, translation), 2), filler), 1)
+    # print (rotMat.shape, pose_matrix.shape)
+    return rotMat, pose_matrix
 
 def mat2euler(M, cy_thresh=None, seq='zyx'):
     '''
@@ -609,18 +615,33 @@ def mat2euler(M, cy_thresh=None, seq='zyx'):
             cy_thresh = np.finfo(M.dtype).eps * 4
         except ValueError:
             cy_thresh = _FLOAT_EPS_4
-    r11, r12, r13, r21, r22, r23, r31, r32, r33 = M.flat
+    M = torch.from_numpy(M)
+    # r11, r12, r13, r21, r22, r23, r31, r32, r33 = M.flat
+    r11 = M[:,0,0]
+    r12 = M[:,0,1]
+    r13 = M[:,0,2]
+    r21 = M[:,1,0]
+    r22 = M[:,1,1]
+    r23 = M[:,1,2]
+    r31 = M[:,2,0]
+    r32 = M[:,2,1]
+    r33 = M[:,2,2]
+
     # cy: sqrt((cos(y)*cos(z))**2 + (cos(x)*cos(y))**2)
-    cy = math.sqrt(r33 * r33 + r23 * r23)
+
+    cy = torch.sqrt((r33 * r33 + r23 * r23))
+    # print (cy.shape)
+    # xxx
     if seq == 'zyx':
-        if cy > cy_thresh:  # cos(y) not close to zero, standard form
-            z = math.atan2(-r12, r11)  # atan2(cos(y)*sin(z), cos(y)*cos(z))
-            y = math.atan2(r13, cy)  # atan2(sin(y), cy)
-            x = math.atan2(-r23, r33)  # atan2(cos(y)*sin(x), cos(x)*cos(y))
+        # if cy > cy_thresh: 
+        if 1:  # cos(y) not close to zero, standard form
+            z = torch.atan2(-r12, r11)  # atan2(cos(y)*sin(z), cos(y)*cos(z))
+            y = torch.atan2(r13, cy)  # atan2(sin(y), cy)
+            x = torch.atan2(-r23, r33)  # atan2(cos(y)*sin(x), cos(x)*cos(y))
         else:  # cos(y) (close to) zero, so x -> 0.0 (see above)
             # so r21 -> sin(z), r22 -> cos(z) and
-            z = math.atan2(r21, r22)
-            y = math.atan2(r13, cy)  # atan2(sin(y), cy)
+            z = torch.atan2(r21, r22)
+            y = torch.atan2(r13, cy)  # atan2(sin(y), cy)
             x = 0.0
     elif seq == 'xyz':
         if cy > cy_thresh:
@@ -637,9 +658,143 @@ def mat2euler(M, cy_thresh=None, seq='zyx'):
     else:
         raise Exception('Sequence not recognized')
     return z, y, x
+# def quat2mat(q):
+#     ''' Calculate rotation matrix corresponding to quaternion
+#     https://afni.nimh.nih.gov/pub/dist/src/pkundu/meica.libs/nibabel/quaternions.py
+#     Parameters
+#     ----------
+#     q : 4 element array-like
+
+#     Returns
+#     -------
+#     M : (3,3) array
+#       Rotation matrix corresponding to input quaternion *q*
+
+#     Notes
+#     -----
+#     Rotation matrix applies to column vectors, and is applied to the
+#     left of coordinate vectors.  The algorithm here allows non-unit
+#     quaternions.
+
+#     References
+#     ----------
+#     Algorithm from
+#     http://en.wikipedia.org/wiki/Rotation_matrix#Quaternion
+
+#     Examples
+#     --------
+#     >>> import numpy as np
+#     >>> M = quat2mat([1, 0, 0, 0]) # Identity quaternion
+#     >>> np.allclose(M, np.eye(3))
+#     True
+#     >>> M = quat2mat([0, 1, 0, 0]) # 180 degree rotn around axis 0
+#     >>> np.allclose(M, np.diag([1, -1, -1]))
+#     True
+#     '''
+#     w, x, y, z = q
+#     Nq = w * w + x * x + y * y + z * z
+#     if Nq < 1e-8:
+#         return np.eye(3)
+#     s = 2.0 / Nq
+#     X = x * s
+#     Y = y * s
+#     Z = z * s
+#     wX = w * X
+#     wY = w * Y
+#     wZ = w * Z
+#     xX = x * X
+#     xY = x * Y
+#     xZ = x * Z
+#     yY = y * Y
+#     yZ = y * Z
+#     zZ = z * Z
+#     return np.array([[1.0 - (yY + zZ), xY - wZ, xZ + wY],
+#                      [xY + wZ, 1.0 - (xX + zZ), yZ - wX],
+#                      [xZ - wY, yZ + wX, 1.0 - (xX + yY)]])
 
 
-import functools
+# def mat2euler(M, cy_thresh=None, seq='zyx'):
+#     '''
+#     Taken From: http://afni.nimh.nih.gov/pub/dist/src/pkundu/meica.libs/nibabel/eulerangles.py
+#     Discover Euler angle vector from 3x3 matrix
+#     Uses the conventions above.
+#     Parameters
+#     ----------
+#     M : array-like, shape (3,3)
+#     cy_thresh : None or scalar, optional
+#      threshold below which to give up on straightforward arctan for
+#      estimating x rotation.  If None (default), estimate from
+#      precision of input.
+#     Returns
+#     -------
+#     z : scalar
+#     y : scalar
+#     x : scalar
+#      Rotations in radians around z, y, x axes, respectively
+#     Notes
+#     -----
+#     If there was no numerical error, the routine could be derived using
+#     Sympy expression for z then y then x rotation matrix, which is::
+#     [                       cos(y)*cos(z),                       -cos(y)*sin(z),         sin(y)],
+#     [cos(x)*sin(z) + cos(z)*sin(x)*sin(y), cos(x)*cos(z) - sin(x)*sin(y)*sin(z), -cos(y)*sin(x)],
+#     [sin(x)*sin(z) - cos(x)*cos(z)*sin(y), cos(z)*sin(x) + cos(x)*sin(y)*sin(z),  cos(x)*cos(y)]
+#     with the obvious derivations for z, y, and x
+#      z = atan2(-r12, r11)
+#      y = asin(r13)
+#      x = atan2(-r23, r33)
+#     for x,y,z order
+#     y = asin(-r31)
+#     x = atan2(r32, r33)
+#     z = atan2(r21, r11)
+#     Problems arise when cos(y) is close to zero, because both of::
+#      z = atan2(cos(y)*sin(z), cos(y)*cos(z))
+#      x = atan2(cos(y)*sin(x), cos(x)*cos(y))
+#     will be close to atan2(0, 0), and highly unstable.
+#     The ``cy`` fix for numerical instability below is from: *Graphics
+#     Gems IV*, Paul Heckbert (editor), Academic Press, 1994, ISBN:
+#     0123361559.  Specifically it comes from EulerAngles.c by Ken
+#     Shoemake, and deals with the case where cos(y) is close to zero:
+#     See: http://www.graphicsgems.org/
+#     The code appears to be licensed (from the website) as "can be used
+#     without restrictions".
+#     '''
+#     M = np.asarray(M)
+#     if cy_thresh is None:
+#         try:
+#             cy_thresh = np.finfo(M.dtype).eps * 4
+#         except ValueError:
+#             cy_thresh = _FLOAT_EPS_4
+#     r11, r12, r13, r21, r22, r23, r31, r32, r33 = M.flat
+#     # cy: sqrt((cos(y)*cos(z))**2 + (cos(x)*cos(y))**2)
+#     cy = math.sqrt(r33 * r33 + r23 * r23)
+#     if seq == 'zyx':
+#         if cy > cy_thresh:  # cos(y) not close to zero, standard form
+#             z = math.atan2(-r12, r11)  # atan2(cos(y)*sin(z), cos(y)*cos(z))
+#             y = math.atan2(r13, cy)  # atan2(sin(y), cy)
+#             x = math.atan2(-r23, r33)  # atan2(cos(y)*sin(x), cos(x)*cos(y))
+#         else:  # cos(y) (close to) zero, so x -> 0.0 (see above)
+#             # so r21 -> sin(z), r22 -> cos(z) and
+#             z = math.atan2(r21, r22)
+#             y = math.atan2(r13, cy)  # atan2(sin(y), cy)
+#             x = 0.0
+#     elif seq == 'xyz':
+#         if cy > cy_thresh:
+#             y = math.atan2(-r31, cy)
+#             x = math.atan2(r32, r33)
+#             z = math.atan2(r21, r11)
+#         else:
+#             z = 0.0
+#             if r31 < 0:
+#                 y = np.pi / 2
+#                 x = atan2(r12, r13)
+#             else:
+#                 y = -np.pi / 2
+#     else:
+#         raise Exception('Sequence not recognized')
+#     return z, y, x
+
+
+# import functools
 
 
 # def euler2mat(z=0, y=0, x=0, isRadian=True):
